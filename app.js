a51978919e9ffd8aa22f1891bc570940edf82873
jamesroyston <jamesroyston03@gmail.com>
2018-12-23 14:16:27 -0700

    added dynamically added buttons, button clicks run model.playRound() and assign model.gameData.userInput to the button choice

--- app.js
+++ app.js
@@ -1,87 +1,171 @@
-const game = function () {
+/*****************************
+   *
+    Rock Paper Scissors
+    Coded by: James Royston III
+    Completed:
+    for The Odin Project
+   *
+ *
+******************************/
 
-  let gameData = {
+const init = (function () {
 
-    playerScore: 0,
-    computerScore: 0,
-    gameCnt: 1,
-    maxRounds: 5,
+
+  const initialize = () => {
+    view.renderButtons();
+    view.renderScoreboard();
   }
 
-  
-  
-  const computerPlay = () => {
-    // random returns rock, paper, or scissors
-    const choice = ['rock', 'paper', 'scissors'];
+  const model = {
 
-    let n = Math.floor(Math.random() * choice.length);
-    return choice[n];
-  }
-  
-  const playRound = (playerSelection, computerSelection) => {
+    // home of our data
+
+    gameData: {
+      playerScore: 0,
+      computerScore: 0,
+      gameCnt: 1,
+      maxRounds: 5,
+      userInput: '',
+    },
+
+    computerPlay: function () {
+      // random returns rock, paper, or scissors
+      const choice = ['rock', 'paper', 'scissors'];
+
+      let n = Math.floor(Math.random() * choice.length);
+      return choice[n];
+    },
+
+    playRound: function (playerSelection, computerSelection) {
     
-    // getCounts();
-    playerSelection = getInput();
-    computerSelection = computerPlay();
+      // getCounts();
+      playerSelection = this.gameData.userInput;
+      computerSelection = this.computerPlay();
 
-    console.log('')
-    console.log('----------')
-    console.log(`Round ${gameData.gameCnt}`)
-    if (playerSelection == computerSelection) {
-      console.log(playerSelection + ' and ' + computerSelection + 
-        ' are the same, no winner')
-    } else if (playerSelection == 'rock') {
-        if (computerSelection == 'paper') {
-          gameData.computerScore++
-          console.log('You lose, paper beats rock')
-        } else if (computerSelection == 'scissors') {
-          gameData.playerScore++
-          console.log('You win, rock beats scissors')
+      console.log('')
+      console.log('----------')
+      console.log(`Round ${this.gameData.gameCnt}`)
+      if (playerSelection == computerSelection) {
+        console.log(playerSelection + ' and ' + computerSelection + 
+          ' are the same, no winner')
+      } else if (playerSelection == 'rock') {
+          if (computerSelection == 'paper') {
+            this.gameData.computerScore++
+            console.log('You lose, paper beats rock')
+          } else if (computerSelection == 'scissors') {
+            this.gameData.playerScore++
+            console.log('You win, rock beats scissors')
+          }
         }
-      }
-
-      else if (playerSelection == 'scissors') {
-        if (computerSelection == 'paper') {
-          gameData.playerScore++
-          console.log('You win, scissors beats paper')
-        } else if (computerSelection = 'rock') {
-          gameData.computerScore++
-          console.log('You lose, rock beats scissors')
+  
+        else if (playerSelection == 'scissors') {
+          if (computerSelection == 'paper') {
+            this.gameData.playerScore++
+            console.log('You win, scissors beats paper')
+          } else if (computerSelection == 'rock') {
+            this.gameData.computerScore++
+            console.log('You lose, rock beats scissors')
+          }
         }
-      }
+        
+        else if (playerSelection == 'paper') {
+          if (computerSelection == 'rock') {
+            this.gameData.playerScore++
+            console.log('You win, paper beats rock')
+          } else if (computerSelection == 'scissors') {
+            this.gameData.computerScore++
+            console.log('You lose, scissors beats paper')
+          }
+        }
+      this.gameData.gameCnt++
+  
       
-      else if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') {
-          gameData.playerScore++
-          console.log('You win, paper beats rock')
-        } else if (computerSelection == 'scissors') {
-          gameData.computerScore++
-          console.log('You lose, scissors beats paper')
+      console.log(`Your score: ${this.gameData.playerScore}`)
+      console.log(`Computer's score: ${this.gameData.computerScore}`)
+      console.log('----------')
+      console.log('')
+  
+      if (this.gameData.gameCnt <= this.gameData.maxRounds) {
+        console.log('awaiting user input')
+      } else {
+        if (this.gameData.playerScore > this.gameData.computerScore) {
+          console.log(`Player wins with a score of ${this.gameData.playerScore}`)
+        } else if (this.gameData.playerScore < this.gameData.computerScore) {
+          console.log(`Computer wins with a score of ${this.gameData.computerScore}`)
         }
       }
-    gameData.gameCnt++
+    },
 
-    console.log(`Your score: ${gameData.playerScore}`)
-    console.log(`Computer's score: ${gameData.computerScore}`)
-    console.log('----------')
-    console.log('')
 
-    if (gameData.gameCnt <= gameData.maxRounds) {
-      playRound();
-    } else {
-      if (gameData.playerScore > gameData.computerScore) {
-        console.log(`Player wins with a score of ${gameData.playerScore}`)
-      } else if (gameData.playerScore < gameData.computerScore) {
-        console.log(`Computer wins with a score of ${gameData.computerScore}`)
-      }
+  }
+  
+  
+  
+  const view = {
+    
+    // home of our views/UI
+    DOMstrings: {
+      app: document.querySelector('#app')
+    },
+
+    renderButtons: function () {
+      
+      // grab the injection point in the DOM
+      
+
+      // create Elements for buttons
+      const rock = document.createElement('button')
+      const paper = document.createElement('button')
+      const scissors = document.createElement('button')
+
+      rock.textContent = 'Rock'
+      paper.textContent = 'Paper'
+      scissors.textContent = 'Scissors'
+
+      const buttons = [rock, paper, scissors]
+      buttons.forEach(buttons => {
+        this.DOMstrings.app.appendChild(buttons)
+      })
+
+      rock.addEventListener('click', controller.chooseRock)
+      paper.addEventListener('click', controller.choosePaper)
+      scissors.addEventListener('click', controller.chooseScissors)
+
+    },
+
+    renderScoreboard: function () {
+      const scoreboard = document.createElement('div');
+      scoreboard.id = 'scoreboard'
+
+      this.DOMstrings.app.appendChild(scoreboard)
+
+      
+    }
+
+  }
+
+  
+  
+  
+  const controller = {
+
+    // home of the interactions between the view/controller
+
+    chooseRock: function() {
+      model.gameData.userInput = 'rock';
+      model.playRound();
+    },
+    choosePaper: function() {
+      model.gameData.userInput = 'paper';
+      model.playRound();
+    },
+    chooseScissors: function() {
+      model.gameData.userInput = 'scissors';
+      model.playRound();
     }
   }
 
-  const getInput = () => {
-    let userInput = prompt('rock, paper, or scissors?').toLowerCase();
-    return userInput;
-  }
-  console.log(playRound())
-}
+  initialize();
+})();
 
-window.onload = console.log(game())
+window.onload = console.log(init);
